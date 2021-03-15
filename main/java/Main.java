00d252357a6c3928ec1514221c68e3616f628f6e
Bing Li <bing.li13@imperial.ac.uk>
2021-03-15 09:59:13 +0000

    test

--- main/java/Main.java
+++ main/java/Main.java
@@ -1,7 +1,14 @@
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        List<Integer> streamIterated = Stream.iterate(40, n -> n + 2).limit(20).collect(toList());
+        System.out.println(streamIterated);
     }
 }
